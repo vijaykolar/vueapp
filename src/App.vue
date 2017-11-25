66527e2722b9630b1092c680902931d85fc791f8
vijayikolar <vijay@lollypop.biz>
2017-11-25 11:43:19 +0530

    Added project setup and Completed Header part

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="app">
+
+        <NavBar/>
+    </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar'
+
+export default {
+    name: 'app',
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style>
+    body,
+    body * {
+        margin: 0px;
+        padding: 0px;
+        font-family: "Lato-Regular",sans-serif;
+    }
+
+    .container {
+        padding: 0 15px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    a {
+        display: inline-block;
+        text-decoration: none;
+    }
+
+    @media(min-width: 768px) {
+        .container {
+            width: 750px;
+        }
+    }
+    @media(min-width: 992px) {
+        .container {
+            width: 970px;
+        }
+    }
+    @media(min-width: 1200px) {
+        .container {
+            width: 1170px;
+        }
+    }
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,85 @@
+<template>
+    <nav class="navbar-menu navbar-fixed">
+        <div class="container">
+            <div class="brand-logo">
+                <a href="javascript:void(0)">HeyU</a>
+            </div>
+            <ul class="navbar navbar-right"> 
+                <li v-for="item in items" class="navbar__list"> 
+                    <a href="javascript:void(0)" class="navbar__link">{{ item.list }}</a>
+                </li>
+            </ul>
+        </div>
+       
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+        data () {
+        return {
+            items: [
+                {list: 'home'},
+                {list: 'faq'},
+                {list: 'support'},
+                {list: 'blog'}
+            ]
+        }
+    }
+
+    
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    /* .navbar-menu {
+        padding: 10px 0px;
+    } */
+
+    .navbar-fixed {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+    }
+
+    .brand-logo {
+        float: left;
+    }
+    .brand-logo>a {
+        padding: 10px 15px;
+        font-weight: 800;
+        color: #fff;
+        color: #000;
+        font-size: 30px;
+    }
+
+    .navbar-right {
+
+        float: right;
+        
+    }
+
+    .navbar__list {
+        display: inline-block;
+    }
+
+    .navbar__link {
+        font-size: 14px;
+        color: #fff;
+        color: #000;
+        font-family: "Lato-Regular",sans-serif;
+        padding: 23px 15px; 
+        text-transform: uppercase;
+        
+
+    }
+
+    
+
+
+</style>
