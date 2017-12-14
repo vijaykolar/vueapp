3b692a2d162c5311f56cc8f3249c967b17b24430
vijayikolar <vijay@lollypop.biz>
2017-12-14 10:11:13 +0530

    Completed Features section

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,9 +1,34 @@
 <template>
     <section class="section section-features">
         <div class="container">
-            <div class="features-wrapper">
+            <div class="features-wrapper ">
+                <div class="clearfix chat-list">
+                    <div class="chat-img-wrapper col-6">
+                        <img src="../assets/chat.png" :alt="alt"/>
+                    </div>
 
-                
+                    <div class="chat-message-info col-6">
+
+                        <h1 class="title title--lg title--black">{{message}}</h1>
+                        <p class="info info--gray"> {{info}}</p>
+                    </div>
+                </div>
+
+                <div class="clearfix chat-list">
+                    <div class="chat-message-info padding col-6">
+
+                        <h1 class="title title--lg title--black">{{message2}}</h1>
+                        <p class="info info--gray"> {{info}}</p>
+                    </div>
+                    <div class="chat-img-wrapper col-6">
+                        <img src="../assets/chat-2.png" :alt="alt"/>
+                    </div>
+
+                    
+                </div>
+               
+               
+               
             </div>
         </div>
     </section>
@@ -13,14 +38,32 @@
 <script>
 export default {
     name: 'Features',
+
+    data() {
+        return {
+
+            
+            alt: "Chat Message",
+            message: "Easy Messaging",
+            message2: "Keep in touch",
+            info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+        }
+    }
    
 }
+
+// new Vue({
+//     name: 'Features',
+
+// })
 </script>
 
 <style scoped>
     .section-features {
         background-color: #fff;
         position: relative;
+        padding-top: 100px;
     }
 
     .section-features:before {
@@ -35,7 +78,32 @@ export default {
 
     }
 
+    .padding .info{
+        padding-right: 100px;
+    }
+
     .features-wrapper {
         position: relative;
     }
+    .chat-message-info .info {
+         
+        margin-top: 30px;
+        
+    }
+     .chat-message-info {
+         
+        margin-top: 150px;
+        
+    }
+
+    .chat-list {
+        margin-top: 100px;
+    }
+    .chat-list:first-child {
+        margin-top: 0px;
+    }
+
+    /* .chat-messge-info-content {
+        padding-left: 25px;
+    } */
 </style>
