f92453da0db98eee363a4908dd34408c3a2487ad
Vijay Kolar <vijayikolar@gmail.com>
2017-12-27 09:42:33 +0530

    Working on Highligths section

--- src/components/HighLigth.vue
+++ src/components/HighLigth.vue
@@ -4,13 +4,36 @@
        <div class="container">
            <div class="highligth-wrapper clearfix">
 
-                <div class="col-4">
-                    <div class="highlight-info">
-                        <h3>Start conversation</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicingsdf elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu. </p>
+               <!--Left Content-->
+                <div class="col-4" >
+                    <div class="highlight-info" v-for='info in chatInfo' >
+                        <h3>{{info.msg}}</h3>
+                        <p>{{info.content}} </p>
                     </div>
 
                     <img src="../assets/chat-bubble.png" alt="chat-bubble">
+                </div>
+
+                <!-- Phone -->
+                <div class="col-4">
+
+                    <div class="phone">
+                        <div class="phone-container">
+
+                            <img src="../assets/phone.png" alt="iPhone" class="img-responsive">
+                            <img src="../assets/play.png" alt="Play Icon" class="play">
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- Right Content-->
+
+                <div class="col-4">
+                    <div class="highlight-info" v-for='info in msgInfo' >
+                        <h3>{{info.msg}}</h3>
+                        <p>{{info.content}} </p>
+                    </div>
                 </div>
 
            </div>
@@ -23,7 +46,16 @@
         name: 'HighLigth',
         data () {
         return {
+            chatInfo: [
+                {msg: 'Start conversation', content:'Lorem ipsum dolor sit amet, consectetur adipisicingsdf elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.' },
+                {msg: 'make a call', content:'Lorem ipsum dolor sit amet, consectetur adipisicingsdf elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.' },
+            ],
 
+            msgInfo : [
+
+                {msg: 'take a picture', content:'Lorem ipsum dolor sit amet, consectetur adipisicingsdf elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.' },
+                {msg: 'send a video', content:'Lorem ipsum dolor sit amet, consectetur adipisicingsdf elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.' }
+            ]
         }
     }
 
@@ -35,5 +67,28 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+    .HighLigth {
+        background:  linear-gradient(to top, #eb402c 0%, #d90647 100%);
+    }
+    .phone-container {
+        position: relative;
+        overflow: hidden;
+    }
 
+    .phone-container::before {
+        /* position: absolute;
+        height: 100%;
+        width: 100%;
+        content: '';
+        background-color: rgba(0,0,0,.6);
+        top: 0px;
+        left: 0px; */
+    }
+    .play {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        cursor: pointer;
+    }
 </style>
